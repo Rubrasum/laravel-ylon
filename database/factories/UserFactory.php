382916272c0c0de7ad046a979250adbc17834433
Rubrasum <josephbetbeze@gmail.com>
2024-04-16 18:02:41 -0400

    First run through encrypting the ssn, just did on the user factory for now.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,22 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // encrypting the ssn
+        $ssn = fake()->numerify('#########');
+        $method = "aes-256-cbc";
+        $iv_length = openssl_cipher_iv_length($method);
+        $iv = openssl_random_pseudo_bytes($iv_length);
+
+        $first_encrypted = openssl_encrypt($ssn,$method, env('APP_BC_K'), OPENSSL_RAW_DATA ,$iv);
+        $second_encrypted = hash_hmac('sha3-512', env('APP_BC_K'), env('APP_BC_KT'), TRUE);
+
+        $ssn = base64_encode($iv.$second_encrypted.$first_encrypted);
+
         return [
             'firstname' => fake()->firstName(),
             'lastname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'social_security' => fake()->numerify('#########'),
+            'social_security' => $ssn,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
