f2561eddf2e8696d0207ef36b101079466395d29
Rubrasum <josephbetbeze@gmail.com>
2024-04-16 17:22:19 -0400

    Adding some margins to the form.

--- resources/views/forms/new-people.blade.php
+++ resources/views/forms/new-people.blade.php
@@ -6,13 +6,13 @@
         @csrf
 
         <!-- Name -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="firstname" :value="__('First Name')" />
             <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
             <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
         </div>
 
-        <div>
+        <div class="mt-4">
             <x-input-label for="lastname" :value="__('Last Name')" />
             <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" />
             <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
@@ -32,13 +32,10 @@
             <x-input-error :messages="$errors->get('social_security')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex items-center justify-center mt-4 mb-4">
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Submit') }}
             </x-primary-button>
         </div>
     </form>
