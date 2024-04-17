5a5304e6f81374bbcc792d443b500eb089a6794a
Rubrasum <josephbetbeze@gmail.com>
2024-04-16 20:04:17 -0400

    Setup the dashboard view to accept and view users in a table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,57 @@
             </div>
         </div>
     </div>
+    <div class="py-12">
+        @if ($users->count() < 2)
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the users including their first name, last name, email and ssn .</p>
+                </div>
+            </div>
+            <div class="mt-8 flow-root">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">First Name</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Last Name</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Active</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                    <span class="sr-only">Toggle Active</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200">
+                            @foreach ($users as $user)
+
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                    {{ $user->firstname }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->lastname }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->email }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->active ? "Active" : "Inactive" }}</td>
+                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Toggle Active<span class="sr-only"></span></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                            <!-- More people... -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @else
+        <div class="flex items-center flex-grow justify-center text-center ">
+            <p class="text-2xl md:text-3xl lg:text-4xl text-white font-semibold py-4">
+                Oops... No users found.
+            </p>
+        </div>
+        @endif
+    </div>
 </x-app-layout>
